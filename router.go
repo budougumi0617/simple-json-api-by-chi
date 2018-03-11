3211dce588f6bb5382fa78e9c3d4f88c9b4c43ed
budougumi0617 <budougumi0617@gmail.com>
2018-03-11 13:49:19 +0900

    :+1: Add logging midleware

--- router.go
+++ router.go
@@ -3,14 +3,29 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 // GetTodoRouter returns simple JSON API server
 func GetTodoRouter() chi.Router {
 	router := chi.NewRouter()
+	// Set output for logging.
+	middleware.DefaultLogger = middleware.RequestLogger(
+		&middleware.DefaultLogFormatter{
+			Logger:  newLogger(),
+			NoColor: false,
+		})
+	router.Use(middleware.Logger)
 	router.HandleFunc("/*", Index) // WildCard.
 	router.HandleFunc("/todos", TodoIndex)
 	router.HandleFunc("/todos/{todoID}", TodoShow)
 	return router
+}
+
+func newLogger() *log.Logger {
+	return log.New(os.Stdout, "chi-log: ", log.Lshortfile)
 }
