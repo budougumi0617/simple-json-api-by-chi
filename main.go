09a20b7b72595a371aeab14999c0111fcadf06aa
budougumi0617 <budougumi0617@gmail.com>
2018-03-11 11:50:32 +0900

    :+1: Add basic HTTP server

---
+++ main.go
@@ -0,0 +1,19 @@
+// Copyright © 2018 budougumi0617 All Rights Reserved.
+
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
