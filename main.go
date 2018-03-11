884e07efb46f81bc589a50ee398e54347c5232f1
budougumi0617 <budougumi0617@gmail.com>
2018-03-11 12:33:27 +0900

    :+1: Add dep setting and set docgen

--- main.go
+++ main.go
@@ -7,13 +7,31 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/docgen"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := chi.NewRouter()
+	router.Get("/*", Index) // *つけないと全てのPATHを受けない。
+	// ルーティング情報を自動生成
+	fmt.Println(docgen.MarkdownRoutesDoc(router, newMarkdownOpts()))
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+// Index writes simple response
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
+func newMarkdownOpts() docgen.MarkdownOpts {
+	return docgen.MarkdownOpts{
+		ProjectPath:        "github.com/budougumi0617/simple-json-api-by-chi",
+		Intro:              "Sample JSON API server by go-chi.",
+		ForceRelativeLinks: true,
+		URLMap: map[string]string{
+			"github.com/my/package/vendor/go-chi/chi/": "https://github.com/go-chi/chi/blob/master/",
+		},
+	}
 }
